3686785d1d9aec411df4e39fae6f5e686d88b425
yendiDev <kceequan01@gmail.com>
2020-05-09 18:14:01 +0000

    Initial commit

---
+++ lib/Quote.dart
@@ -0,0 +1,6 @@
+
+class Quote {
+  String text, author;
+
+  Quote({this.text, this.author});
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:quotes/Quote.dart';
+
+void main() => runApp(new MaterialApp(
+  home: QuoteList(),
+));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<Quote> quotes = [
+    Quote(text: 'Be yourself: everyone else is taken', author: 'Oscar Wilde'),
+    Quote(text: 'Be yourself: everyone else is taken', author: 'Oscar Wilde'),
+    Quote(text: 'Be yourself: everyone else is taken', author: 'Oscar Wilde'),
+
+  ];
+
+  Widget quoteTemplate(quote){
+    return Card(
+      margin: new EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+
+            Text(quote.text,
+            style: TextStyle(
+              fontSize: 18.0,
+              color: Colors.grey[600]
+            ),),
+
+            SizedBox(height: 6.0,),
+
+            Text(quote.author,
+            style: TextStyle(
+              fontSize: 14.0,
+              color: Colors.grey[800]
+            ),),
+
+          ],
+        ),
+      ),
+
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200] ,
+      appBar: AppBar(
+        title: new Text("Awesome Qoutes"),
+        backgroundColor: Colors.redAccent,
+        centerTitle: true,
+      ),
+
+      body: Column(
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+
+      ),
+
+    );
+  }
+}
